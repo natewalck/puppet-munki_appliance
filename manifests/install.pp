e99f6578ceffe49f6aaab84499e9d2d024240d3f
natewalck <nate@osxadm.in>
2013-08-04 19:58:24 -0400

    Added python class

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,5 +1,13 @@
 # /etc/puppet/modules/munki_appliance/manifests/install.pp
 
 class munki_appliance::install {
+
   class { 'apache': default_vhost => false, }
+
+  package { 'git': ensure => present, }
+
+  class { 'python':
+    version => 'system',
+    virtualenv => true,
+  }
 }
