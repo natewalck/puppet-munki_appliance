006521c8404b7604ba8b94baae6ca065c9e54504
natewalck <nate@osxadm.in>
2013-08-04 22:08:04 -0400

    prestaged opt file

--- manifests/virtualenv.pp
+++ manifests/virtualenv.pp
@@ -1,11 +1,16 @@
 # /etc/puppet/modules/munki_appliance/manifests/virtualenv.pp
 
 class munki_appliance::virtualenv {
+  $munki_web_admin_user = $munki_appliance::munki_web_admin_user
+
+
+  file { '/opt': ensure => directory, owner => $munki_web_admin_user, }
 
   python::virtualenv { '/opt/munkiwebadmin_env':
     ensure  => present,
     version => 'system',
-    owner   => 'munkiwebadmin',
-    group   => 'munkiwebadmin',
+    owner   => $munki_web_admin_user,
+    group   => $munki_web_admin_user,
+    require => File['/opt'],
   }
 }
