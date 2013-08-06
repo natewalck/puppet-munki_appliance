fdf15002d95d51730c1b3135354ac383242ffd9f
natewalck <nate@osxadm.in>
2013-08-05 21:39:11 -0400

    Added script to check if user exists

---
+++ files/doesuserexist.py
@@ -0,0 +1,34 @@
+from django.core.management import setup_environ
+import optparse
+import settings
+
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+setup_environ(settings)
+
+from django.contrib.auth.models import User
+
+
+def main():
+    p = optparse.OptionParser()
+    p.set_usage("""Usage: %prog [options]""")
+    p.add_option('--username', '-u', dest='username',
+                 help="""Username for the superuser.""")
+    p.add_option('--password', '-p', dest='password',
+                 help="""Password for the superuser.""")
+
+    options, arguments = p.parse_args()
+
+    print(username_present(options.username))
+
+
+def username_present(username):
+    if User.objects.filter(username=username).count():
+        return True
+
+    return False
+
+
+if __name__ == '__main__':
+    main()
