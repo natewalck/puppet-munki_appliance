25c14ac84fbdcfbbd299b6ccc0c9197236afbecb
natewalck <nate@osxadm.in>
2013-09-04 17:34:57 -0400

    Add group variable instead of hardcoded group for users

--- manifests/users.pp
+++ manifests/users.pp
@@ -1,17 +1,17 @@
 # /etc/puppet/modules/munki_appliance/manifests/users.pp
 
 class munki_appliance::users {
-
   $munki_user          = $munki_appliance::munki_user
   $mwa_service_account = $munki_appliance::mwa_service_account
+  $munki_group         = $munki_appliance::munki_group
 
   user { $munki_user :
     ensure => present,
-    groups => munki,
+    groups => $munki_group,
   }
 
   user { $mwa_service_account :
     ensure => present,
-    groups => munki,
+    groups => $munki_group,
   }
 }
