bbde7805082d684725175fa8d6daf3af357646bc
groob <vrancean+github@gmail.com>
2013-11-22 14:04:31 -0500

    added the ability to specify a cifs share as a mountpoint for your repo instead of initializing a new repo. The old behaviour is the default. example: class {'munki_appliance':   new_repo  => "mount",   device    => "example.com" #remote server   share     => "munki_repo" #remote share //example.com/share }

--- manifests/munki_dirs.pp
+++ manifests/munki_dirs.pp
@@ -1,25 +1,49 @@
 # /etc/puppet/modules/munki_appliance/manifests/munki_dirs.pp
-
 class munki_appliance::munki_dirs{
+  $new_repo  = $munki_appliance::new_repo
+  $repo_src = $munki_appliance::repo_src
+  $device  = $munki_appliance::device
+  $share  = $munki_appliance::share
+  $admin_password  = $munki_appliance::admin_password
   $munki_root  = $munki_appliance::munki_root
   $munki_user  = $munki_appliance::munki_user
   $munki_group = $munki_appliance::munki_group
   $munki_dirs  = ["${munki_root}/repo/catalogs", "${munki_root}/repo/manifests",
-                  "${munki_root}/repo/pkgs", "${munki_root}/repo/pkgsinfo"]
+  "${munki_root}/repo/pkgs", "${munki_root}/repo/pkgsinfo"]
 
-  vcsrepo { "${munki_root}/repo" :
-    ensure   => present,
-    provider => git,
-    owner    => $munki_user,
-    group    => $munki_group,
-  }
+  case $new_repo {
+    'mount': {
+      ensure_packages(["cifs-utils", "samba-client", "samba-common"])
+      file { "${munki_root}/repo" :
+      ensure => 'directory',
+      }
+      mount {'munki_repo':
+        atboot   => true,
+        name     => "${munki_root}/repo",
+        device   => "//$device/$share",
+        ensure   => 'mounted',
+        fstype   => 'cifs',
+        options  => "user=$munki_user,password=$admin_password",
+      }
+    }
+    default: {
+      vcsrepo { "${munki_root}/repo" :
+        ensure   => present,
+        provider => git,
+        owner   => $munki_user,
+        group   => $munki_group,
+        source   => "$repo_src"
+      }
 
-  file { $munki_dirs :
-    ensure  => 'directory',
-    recurse => true,
-    owner   => $munki_user,
-    group   => $munki_group,
-    mode    => '0664',
-    require => Vcsrepo["${munki_root}/repo"],
+      file { $munki_dirs :
+        ensure  => 'directory',
+        recurse => true,
+        owner   => $munki_user,
+        group   => $munki_group,
+        mode    => '0664',
+        require => Vcsrepo["${munki_root}/repo"],
+      }
+    }
   }
 }
+
